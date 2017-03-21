0d132e6c8c5aabc0a67bd8202d60471a829bba7d
Terralux <kenneth@berle.eu>
2017-03-21 20:00:00 +0100

    Added initial story system setup

--- Everlie/Assets/Scripts/AcceptReject.cs
+++ Everlie/Assets/Scripts/AcceptReject.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class AcceptReject : MiniGame {
 	
+	#region implemented abstract members of MiniGame
+	public override void Update ()
+	{
+		throw new System.NotImplementedException ();
+	}
+	#endregion
 }
--- Everlie/Assets/Scripts/GameMaster.cs
+++ Everlie/Assets/Scripts/GameMaster.cs
@@ -7,13 +7,11 @@ public class GameMaster : MonoBehaviour {
 	public delegate void voidEvent ();
 	public voidEvent OnStorySegmentEnded;
 
-	private CollectiveStory story;
+	public CollectiveStory story;
 	private int index = 0;
 
 	void Awake () {
 		Toolbox.RegisterComponent<GameMaster> (this);
-
-		story = (CollectiveStory) Resources.Load ("StorySegments");
 		story.Initiate();
 	}
 
